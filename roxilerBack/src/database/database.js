a208915652265fc2ce51a1c0f7b88626094d7547
noobwebdev777 <125580549+noobwebdev777@users.noreply.github.com>
2024-05-05 17:35:40 +0530

    database error lol

--- roxilerBack/src/database/database.js
+++ roxilerBack/src/database/database.js
@@ -1,20 +1,28 @@
 // database.js
 const sqlite3 = require('sqlite3').verbose();
 
-const db = new sqlite3.Database('database.db');
+const db = new sqlite3.Database('products.db');
+
+require('dotenv').config();
+
+const fetch = require('node-fetch');
+
+const json = require('../../product_transaction.json');
+
+const url = process.env.PRIMARY_DATA;
 
 // Create a table
 db.run(
   `CREATE TABLE IF NOT EXISTS products (
-    id INT PRIMARY KEY,
-    title VARCHAR(255),
-    price DECIMAL(10, 2),
+    id INTEGER PRIMARY KEY,
+    title TEXT ,
+    price INTEGER ,
     description TEXT,
-    category VARCHAR(255),
-    image VARCHAR(255),
-    sold BOOLEAN,
-    dateOfSale TIMESTAMP
-)`,
+    category TEXT,
+    image TEXT,
+    sold INTEGER DEFAULT 0,
+    dateOfSale TEXT
+);`,
   (err) => {
     if (err) {
       console.error('Error creating table:', err.message);
@@ -24,12 +32,39 @@ db.run(
   }
 );
 
-const data = require('./dataFetch')
-
-if(data.length === 0){
-  console.log('no data')
-}else{
-  console.log(data)
-}
+const fetchData = fetch(url)
+  .then((response) => {
+    console.log(response.ok);
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    return response.json();
+  })
+  .then((data) => {
+    // console.log(data)
+    for (i = 0; i < data.length; i++) {
+      const {
+        id,
+        title,
+        price,
+        description,
+        category,
+        image,
+        sold,
+        dateOfSale,
+      } = data[i];
+      const qury = `INSERT INTO products (id, title, price, description, category, image, sold, dateOfSale) 
+      VALUES (${id}, ${title}, ${price}, ${description}, ${category}, ${image}, ${sold}, ${dateOfSale});
+      `;
+      db.run(qury, function (err) {
+        if (err) {
+          console.error('Error inserting data:', err.message);
+        } else {
+          console.log(`Row inserted with rowid ${this.lastID}`);
+        }
+      });
+    }
+  })
+  .catch((error) => console.log(error));
 
 module.exports = db;
--- roxilerBack/src/server/server.js
+++ roxilerBack/src/server/server.js
@@ -4,11 +4,30 @@ const app = express();
 
 const db = require('../database/database');
 
+app.use(express.json())
+
 // const data = require('../database/dataFetch');
 
 // console.log(data, 'server data')
 
 const port = process.env.PORT || 5000;
+
+app.get('/' , (req, res) => {
+
+})
+
+app.get('/' , (req, res) => {
+  
+})
+
+app.get('/' , (req, res) => {
+  
+})
+
+app.get('/' , (req, res) => {
+  
+})
+
 
 app.listen(port, () => {
   console.log(`server started at port ${port}`);
