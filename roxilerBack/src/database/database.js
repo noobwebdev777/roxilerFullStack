dc1b4e0de5abe32f28133dbc74544776c2ae848a
noobwebdev777 <webdev.proj.777@gmail.com>
2024-05-06 20:00:45 +0530

    db connection done

--- roxilerBack/src/database/database.js
+++ roxilerBack/src/database/database.js
@@ -1,20 +1,26 @@
 // database.js
-const sqlite3 = require('sqlite3').verbose();
+const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database('products.db');
+const db = new sqlite3.Database("products.db");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-const json = require('../../product_transaction.json');
+const json = require("../../product_transaction.json");
 
 const url = process.env.PRIMARY_DATA;
 
-// Create a table
-db.run(
-  `CREATE TABLE IF NOT EXISTS products (
-    id INTEGER PRIMARY KEY,
+db.all(`SELECT COUNT(*) AS total_rows FROM products;`, (err, rows) => {
+  if (err) {
+    console.error("Error executing query:", err.message);
+  } else {
+    const totalRows = rows[0].total_rows;
+    if (totalRows === 0) {
+      // Create a table
+      db.run(
+        `CREATE TABLE IF NOT EXISTS products (
+    id INTEGER,
     title TEXT ,
     price INTEGER ,
     description TEXT,
@@ -23,48 +29,69 @@ db.run(
     sold INTEGER DEFAULT 0,
     dateOfSale TEXT
 );`,
-  (err) => {
-    if (err) {
-      console.error('Error creating table:', err.message);
+        (err) => {
+          if (err) {
+            console.error("Error creating table:", err.message);
+          } else {
+            console.log("Users table created successfully.");
+          }
+        }
+      );
+
+      const fetchData = fetch(url)
+        .then((response) => {
+          console.log(response.ok);
+          if (!response.ok) {
+            throw new Error("Failed to fetch data");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          function convertToSingleQuotes(str) {
+            return str.replace(/"/g, "'");
+          }
+          // console.log(data)
+          for (i = 0; i < data.length; i++) {
+            const {
+              id,
+              title,
+              price,
+              description,
+              category,
+              image,
+              sold,
+              dateOfSale,
+            } = data[i];
+
+            // Function to properly format strings for SQL queries (escaping single quotes)
+            function convertToSingleQuotes(str) {
+              return str.replace(/'/g, "''");
+            }
+
+            let stringWithSingleQuotes = convertToSingleQuotes(title);
+
+            console.log(stringWithSingleQuotes);
+
+            let cat = convertToSingleQuotes(category);
+
+            const query = `INSERT INTO products (id, title, price, description, category, image, sold, dateOfSale)
+                     VALUES (${id}, '${stringWithSingleQuotes}', ${price}, '${description}', '${cat}', '${image}', ${sold}, '${dateOfSale}');
+                   `;
+
+            db.run(query, function (err) {
+              if (err) {
+                console.error("Error inserting data:", err.message);
+              } else {
+                console.log(`Row inserted with rowid ${this.lastID}`);
+              }
+            });
+          }
+        })
+        .catch((error) => console.log(error));
     } else {
-      console.log('Users table created successfully.');
+      console.log("product_db have data, no need to fetch data");
     }
   }
-);
-
-const fetchData = fetch(url)
-  .then((response) => {
-    console.log(response.ok);
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    return response.json();
-  })
-  .then((data) => {
-    // console.log(data)
-    for (i = 0; i < data.length; i++) {
-      const {
-        id,
-        title,
-        price,
-        description,
-        category,
-        image,
-        sold,
-        dateOfSale,
-      } = data[i];
-      const qury = `INSERT INTO products (id, title, price, description, category, image, sold, dateOfSale) 
-      VALUES (${id}, ${title}, ${price}, ${description}, ${category}, ${image}, ${sold}, ${dateOfSale});
-      `;
-      db.run(qury, function (err) {
-        if (err) {
-          console.error('Error inserting data:', err.message);
-        } else {
-          console.log(`Row inserted with rowid ${this.lastID}`);
-        }
-      });
-    }
-  })
-  .catch((error) => console.log(error));
+});
 
 module.exports = db;
--- roxilerBack/src/server/server.js
+++ roxilerBack/src/server/server.js
@@ -1,33 +1,56 @@
-const express = require('express');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const app = express();
 
-const db = require('../database/database');
+const db = require("../database/database");
 
-app.use(express.json())
-
-// const data = require('../database/dataFetch');
-
-// console.log(data, 'server data')
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
-app.get('/' , (req, res) => {
+//search and pegnation params api
+app.get("/transactiontable", (req, res) => {
+  const {search, offset} = req.query
+  let query = ``
 
-})
+  console.log(search)
 
-app.get('/' , (req, res) => {
-  
-})
+  if (search == '**'){
+    query = `select * from products limit 10 offset ${offset}`
+  }else{
+    query = `SELECT * FROM products WHERE title LIKE '%${search}%' OR description LIKE '%${search}%' OR price LIKE '%${search}%' 
+  limit 10 offset ${offset};`
+  }
 
-app.get('/' , (req, res) => {
-  
-})
+  console.log(query)
 
-app.get('/' , (req, res) => {
-  
-})
+  db.all(query, (err, rows) => {
+    if(err){
+      res.status(500).json({message: err})
+    }else{
+      res.status(200).json(rows)
+    }
+  })
 
+  // res.status(200).json({message: 'got the request lmao'})
+});
+
+//statistics and sales data api
+app.get("/statistics/:month", (req, res) => {
+  const month = req.params.month
+  const query = `select sum(price) as total_sale, count(id) as total_sale from products where substr(dateOfSale, 6, 2) = ${month} group by sold;`
+  db.run(query, (err, rows) => {
+    if(err){
+      res.status(500).json({message: 'contact me on github @noobwebdev777'})
+    }else{
+      console.log(rows)
+    }
+  })
+});
+
+app.get("/", (req, res) => {});
+
+app.get("/", (req, res) => {});
 
 app.listen(port, () => {
   console.log(`server started at port ${port}`);
